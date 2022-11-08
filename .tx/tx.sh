63b66b61cfa9cf42136eba83335165c55cb62101
Robert Göhler <github@ghlr.de>
2022-11-08 22:00:00 +0100

    Update tx.sh

--- .tx/tx.sh
+++ .tx/tx.sh
@@ -10,7 +10,7 @@ for FILEPATH in $(find docs/_build/gettext -name '*.pot'); do
     fi
 
     RESOURCE_SLUG=$(echo $FILEPATH | sed 's/docs\/_build\/gettext\///' | sed 's/.pot//' | tr "/" "_" )
-    FILE_FILTER=$(echo $FILEPATH | sed 's/build\/gettext/locales\/<lang>\/LC_MESSAGES/' | sed 's/.$//')
+    FILE_FILTER=$(echo $FILEPATH | sed 's/build\/gettext/locale\/<lang>\/LC_MESSAGES/' | sed 's/.$//')
     SOURCE_FILE=$FILEPATH
     
     tx add \
